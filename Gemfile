5212267666ca8462f38a99d4241e833002a5b0d5
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-01-04 20:45:27 -0700

    Initial files added

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+ruby '3.2.0'
+
+gem "sinatra", "~>1.4.7"
+gem "sinatra-contrib"
+gem "erubis"
+gem "rubocop"
---
+++ todo.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/content_for'
+require 'tilt/erubis'
+
+require 'securerandom'
+secret = SecureRandom.hex(32)
+
+configure do
+  enable :sessions
+  set :session_secret, secret
+end
+
+before do
+  session[:lists] ||= []
+  @lists = session[:lists]
+end
+
+# returns an array of list names
+def list_names
+  lists = session[:lists]
+  lists.map { |list| list[:name] }
+end
+
+# returns a boolean representing validity of given list name
+def valid_name?(name)
+  error_message(name) == 'no error'
+end
+
+# returns error message for invalid list name, or "no error" if name is valid
+def error_message(name)
+  if !(1..100).cover?(name.length)
+    return 'List name must be between 1 and 100 characters.'
+  elsif list_names.include?(name)
+    return "'#{name}' is already a list. Name must be unique."
+  end
+
+  'no error'
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# View list of lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists
+end
+
+# Render 'new list' form
+get '/lists/new' do
+  erb :new_list
+end
+
+# get specific list or create new list
+get '/lists/:id' do
+  @list = @lists[params[:id].to_i]
+  erb :list
+end
+
+get '/list/:id/edit' do
+  @id = params[:id].to_i
+  @list = @lists[@id]
+  erb :edit_list
+end
+
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  if valid_name?(list_name)
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  else
+    session[:error] = error_message(list_name)
+    erb :new_list
+  end
+end
+
+# Change list name
+post '/list/:id/edit' do
+  new_list_name = params[:list_name].strip
+  @id = params[:id].to_i
+  @list = @lists[@id]
+  old_name = @list[:name]
+
+  if valid_name?(new_list_name)
+    session[:lists][@id][:name] = new_list_name
+    session[:success] = "The list '#{old_name}' has been renamed to '#{new_list_name}'"
+    redirect '/lists'
+  else
+    session[:error] = error_message(new_list_name)
+    erb :edit_list
+  end
+end
